8fa817884d2c9f7583e5109fb0c71ec6177d227e
Cagatay Berk Cingiz <95578046+haylejj@users.noreply.github.com>
2023-08-03 14:19:30 +0300

    Dashboard page added

---
+++ BlogSite/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ BlogSite/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View(categoryManager.GetList());
+        }
+    }
+}
